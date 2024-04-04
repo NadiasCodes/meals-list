f942d06ceb39f71037bf38678278f0a6230d315e
NadiasCodes <De.nadia@outlook.com>
2024-04-04 10:26:34 +0200

    gave a default style to the app and finished it by setting up the counter to show how many meals are for the day

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useMealsListContext } from '../Provider/MealsProvider'
 
 const Counter = () => {
+    const { meals } = useMealsListContext();
   return (
     <div>
-      
+      <h3>Number of meals today: {meals.length}</h3>
     </div>
   )
 }
--- src/Components/MealsList.jsx
+++ src/Components/MealsList.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useMealsListContext } from "../Provider/MealsProvider";
+import "./MealsList.css";
 
 const MealsList = () => {
-  return <div></div>;
+  const { meals } = useMealsListContext();
+  return (
+    <div className="meals-container">
+      <div className="meals-box">
+        <h1>Meals List using Context API</h1>
+        {meals.map((meal, index) => (
+          <h2 key={index}>{meal}</h2>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MealsList;
